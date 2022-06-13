5a5eb3faad00304da71b014a1ca43f9e2b72aadf
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2022-06-13 20:37:26 +0200

    Answered Questions 1-5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// Question 1
+
+const cat = {
+        complain: function() {
+            console.log("Meow!");
+        }
+    };
+
+cat.complain();
+
+// Question 2, 3 and 4
+
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated Heading"
+heading.style.fontSize = "2em"
+heading.innerHTML += "<h4>subheading</h4>";
+
 const cats = [
     {
         name: "Blob",
